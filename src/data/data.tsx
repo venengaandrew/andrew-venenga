7931ed72449c03d8a7af41d8223a27ad0248f2ae
andrew.venenga <drewvpopc@gmail.com>
2023-09-03 22:36:04 -0500

    testing SVG loading over WEBp

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -14,7 +14,7 @@ import GithubIcon from '../components/Icon/GithubIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
 // import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
 // import TwitterIcon from '../components/Icon/TwitterIcon';
-import heroImage from '../images/header-background.webp';
+import heroImage from '../images/header-background.svg';
 import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
 import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
 // import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
