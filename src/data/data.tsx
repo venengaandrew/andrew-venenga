f087d93fa1cdb198a883c1f7c522dbfc9c1691e0
Andrew Venenga <andrew@venenga.net>
2024-04-28 20:30:55 -0500

    Update data.tsx
    Changed the description in the contact form

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -350,7 +350,7 @@ export const testimonial: TestimonialSection = {
 
 export const contact: ContactSection = {
   headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  description: 'Use this form to get in touch!',
   items: [
     {
       type: ContactType.Email,
