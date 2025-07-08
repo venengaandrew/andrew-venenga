f5b38ae6f7355bef3360364b832bf76a8eef95a6
Andrew Venenga <andrew@venenga.net>
2025-07-08 17:18:49 -0500

    Updated G-Tag

--- src/components/GoogleTagManager.tsx
+++ src/components/GoogleTagManager.tsx
@@ -18,20 +18,20 @@ const GoogleTagManager: React.FC<GoogleTagManagerProps> = memo(({children}) => {
       <noscript>
         <iframe
           height="0"
-          src="https://www.googletagmanager.com/ns.html?id=G-MSSPKY60PN"
+          src="https://www.googletagmanager.com/ns.html?id=G-GLJC44XFWS"
           style={{display: 'none', visibility: 'hidden'}}
           width="0"
         />
       </noscript>
       {/* End Google Tag Manager (body) */}
       {/* Google Analytics */}
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-MSSPKY60PN" strategy="lazyOnload" />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-GLJC44XFWS" strategy="lazyOnload" />
       <Script id="ga-script" strategy="lazyOnload">
         {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-MSSPKY60PN');
+            gtag('config', 'G-GLJC44XFWS');
           `}
       </Script>
       {/* End Google Analytics */}
